57f56eb2f1a43a87ddcd6e312412b09f86b6401d
Erik <60672234+erikoc@users.noreply.github.com>
2025-01-17 12:14:36 +0100

    Cleaning up repository and starting work on voting. Reference geodata set at 2021.

--- src/plot.py
+++ src/plot.py
@@ -7,8 +7,8 @@ from fun import (
 )
 
 # Define file paths
-geo_filepath = '../data/DE_VG250.gpkg'
-religion_filepath = '../data/1000A-1018_en.xlsx'
+geo_filepath = '../data/shapefiles/vg250_ebenen_1231/DE_VG250.gpkg'
+religion_filepath = '../data/zensus/1000A-1018_en.xlsx'
 
 # 1) Load geographic data
 gem_data = load_geodata(geo_filepath)
@@ -22,9 +22,14 @@ map_data_religion = merge_data_religion(gem_data, religion_data)
 # 4) Plot the map for Catholics (no Eichsfeld/Geisa boundaries)
 plot_map_religion(
     map_data_religion,
-    column='Catholic',
-    cmap='Reds',
+    column='Protestant',
+    cmap='Blues',
     legend_label='Catholics %',
     output_folder="../bld/maps",
     filename="catholic_distribution_map.png"
 )
+
+import pyreadr
+
+# Replace 'path_to_file.rds' with the path to your .rds file
+result = pyreadr.read_r('..\\data\\federal_muni_harm.rds')
